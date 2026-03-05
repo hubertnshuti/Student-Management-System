84d8154add9c40deb68fe2a95c66307ffcd72b2e
ngenziwingenzi <nshutihubert4@gmail.com>
2026-03-05 13:47:18 +0200

    Add Main class to implement the program flow

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import dao.StudentDAO;
+import models.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        // Quick model test
+        Student s = new Student(
+                "Hubert Nshuti Ngendahayo",
+                "nshutihubert04@gmail.com",
+                "001",
+                "Computer Science",
+                90.5
+        );
+
+        s.displayInfo();
+
+        // Quick DAO placeholder test
+        StudentDAO dao = new StudentDAO();
+        dao.add();
+        dao.search("001");
+    }
+}
